fe31897241e409f45e0f8f593808b96e8d0726c7
tsflatif <tsflatif@gmail.com>
2016-01-20 10:22:34 -0500

    make templates and add routing

--- www/js/app.js
+++ www/js/app.js
@@ -23,18 +23,44 @@ angular.module('starter', ['ionic'])
   });
 })
 
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('tabs', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+    .state('tabs.list', {
+      url: '/list',
+      views: {
+        'list-tab' : {
+          templateUrl: 'templates/list.html',
+          controller: 'ListController'
+        }
+      }
+    })
+  $urlRouterProvider.otherwise('/tab/list');
+})
+
 .controller('ListController', ['$scope', '$http', function($scope, $http) {
     $http.get('js/data.json').success(function(data) {
       $scope.artists = data;
 
       $scope.onItemDelete = function(item) {
         $scope.artists.splice($scope.artists.indexOf(item),1);
-      }  
+      } 
+
+      $scope.doRefresh =function() {
+      $http.get('js/data.json').success(function(data) {
+          $scope.artists = data;
+          $scope.$broadcast('scroll.refreshComplete'); 
+        });
+      } 
 
       $scope.toggleStar = function(item) {
         item.star = !item.star;
       }
-      
+
       $scope.moveItem = function(item, fromIndex, toIndex){
         $scope.artists.splice(fromIndex, 1);
         $scope.artists.splice(toIndex,0, item); 
